6d28bb5996aeb8ec0319994c139abf0540a9cb92
K.tetta <c0a2305850@edu.teu.ac.jp>
2024-04-23 14:59:22 +0900

    練習3、はみだし設定

--- 逃げろ！こうかとん.py
+++ 逃げろ！こうかとん.py
@@ -13,7 +13,19 @@ DELTA = {  # 移動量辞書
 }# 練習1
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
+def check_bound(obj_rct):
+    """
+    こうかとんRect,または,爆弾Rectの画面外判定用の関数
+    引数：こうかとんRect,または,爆弾Rect
+    戻り値：横方向判定結果, 縦方向判定結果（True：画面内/False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+# 練習3
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -53,9 +65,16 @@ def main():
         #if key_lst[pg.K_RIGHT]:
         #    sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bm_rct.move_ip(vx, vy)
         screen.blit(bm_img, bm_rct)
+        yoko, tate = check_bound(bm_rct)
+        if not yoko:
+            vx *= -1 # 横にはみだし
+        if not tate:
+            vy *= -1 # 縦にはみだし
         pg.display.update()
         tmr += 1
         clock.tick(50)
